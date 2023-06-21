f8939948a44c4c60c909605f65aed2644570b408
John San Soucie <jsansoucie@whoi.edu>
2023-06-21 16:44:19 +0000

    add_ignore CNL100

--- sgdrf/test/test_pep257.py
+++ sgdrf/test/test_pep257.py
@@ -20,6 +20,10 @@ from ament_pep257.main import main
 @pytest.mark.pep257
 def test_pep257():
     rc = main(
-        argv=["--convention=numpy", "--add-ignore=D100,D101,D102,D103,D104", "sgdrf"]
+        argv=[
+            "--convention=numpy",
+            "--add-ignore=D100,D101,D102,D103,D104,CNL100",
+            "sgdrf",
+        ]
     )
     assert rc == 0, "Found code style errors / warnings"
