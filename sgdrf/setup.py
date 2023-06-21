9f701c27d06d8de6192666c286bc07946c742c62
John San Soucie <jsansoucie@whoi.edu>
2023-06-21 13:16:21 +0000

    black formatting

--- sgdrf/setup.py
+++ sgdrf/setup.py
@@ -10,7 +10,10 @@ setup(
     packages=find_packages(exclude=["test"]),
     data_files=[
         (os.path.join("share", package_name), ["package.xml"]),
-        (os.path.join('share', 'ament_index', 'resource_index', 'packages'), [os.path.join('resources', package_name)]),
+        (
+            os.path.join("share", "ament_index", "resource_index", "packages"),
+            [os.path.join("resources", package_name)],
+        ),
         (os.path.join("share", package_name), glob("launch/*launch.[pxy][yma]*")),
     ],
     install_requires=["setuptools", "pyro-ppl", "torch"],
